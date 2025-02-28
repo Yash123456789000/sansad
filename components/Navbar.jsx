00ee9a5d33dd4e8d2c68c2bd9508a6e1802f636d
Yash123456789000 <iamyash@kgpian.iitkgp.ac.in>
2025-02-28 15:03:14 +0530

    sansad

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import logo from "@/public/assets/logos/ppgLogo.svg";
+import logo from "@/public/assets/logos/ppglogo.svg";
 import {
   motion,
   AnimatePresence,
